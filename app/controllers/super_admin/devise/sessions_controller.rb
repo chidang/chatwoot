bb6bf79173b6eb894a683f36f8bf247d9eb99bfd
chidang <dangtanminhchi@gmail.com>
2026-05-05 20:34:09 +0700

    Update brand

--- app/controllers/super_admin/devise/sessions_controller.rb
+++ app/controllers/super_admin/devise/sessions_controller.rb
@@ -3,6 +3,7 @@
 class SuperAdmin::Devise::SessionsController < Devise::SessionsController
   def new
     self.resource = resource_class.new(sign_in_params)
+    @global_config = GlobalConfig.get('INSTALLATION_NAME')
   end
 
   def create
